4d3050e5dec54ede774ba881f71aa30b90ec409c
1 <abdounovich@gmail.com>
2020-09-12 13:26:40 +0200

    'comment'

--- resources/views/rdv.blade.php
+++ resources/views/rdv.blade.php
@@ -48,13 +48,13 @@
   <div class="row">
 
     <div class="col p-4 ">
-        <a class="btn btn-danger p-3 "  onclick="return confirm('هل أنت متأكد ?')" href="/add">مسح المواعيد</a>
+        <a class="btn btn-danger m-2 p-3 "  onclick="return confirm('هل أنت متأكد ?')" href="/add">مسح المواعيد</a>
 
    
-  <a id="button" class=" text-white btn btn-info p-3"> 
+  <a id="button" class=" text-white btn btn-info m-2 p-3"> 
     اعادة تحميل الصفحة
   </a> 
-  <a  href="/types" class=" text-white btn btn-success p-3"> 
+  <a  href="/types" class=" text-white btn btn-success m-2 p-3"> 
 أضف أنواع جديدة   </a> 
 </div>
   </div>
