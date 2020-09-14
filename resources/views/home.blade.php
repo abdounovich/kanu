89d48b674896f191aab3c8d3874bf97450569ff0
1 <abdounovich@gmail.com>
2020-09-15 01:19:01 +0200

    'comment'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,7 +25,7 @@
         </div>
       
 
-        <div class="col col-4 ">  <div class="card rounded bg-success text-white p-2 shadow" style="opacity: 0.8" >
+        <div class="col col-4 ">  <div class="card rounded bg-danger text-white p-2 shadow" style="opacity: 0.8" >
             <i class="m-4 p-2 fa fa-calendar fa-5x d-flex align-self-center "></i>
             <div class="h3 p-2   d-flex align-self-center">عدد المواعيد  : {{$appointments->count()}}</div>
             <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
@@ -34,9 +34,9 @@
 
 
 
-            <div class="col col-4 ">  <div class="card rounded bg-info text-white p-2 shadow" style="opacity: 0.8" >
+            <div class="col col-4 ">  <div class="card rounded bg-success text-white p-2 shadow" style="opacity: 0.8" >
                 <i class="m-4 p-2 fa fa-list fa-5x d-flex align-self-center "></i>
-                <div class="h3 p-2   d-flex align-self-center">عدد الأنواع : {{$types->count()}}</div>
+                <div class="h3 p-4   d-flex align-self-center">عدد الأنواع : {{$types->count()}}</div>
                 <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
                  </div>
                 </div>
