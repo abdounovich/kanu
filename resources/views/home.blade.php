2f2507eb029c6f81c66db683dd52f67f3ab24709
1 <abdounovich@gmail.com>
2020-09-15 01:14:14 +0200

    'comment'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,52 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+@extends('layouts.master')
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-    Launch demo modal
-  </button>
+@section('title', 'Clients')
+
+
+
+@section('content')
+
+
+
+
+
+ 
+
   
-  <!-- Modal -->
-  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+<div class="container">
+    <div class="row">
+
+        <div class="col col-4 p-4">  <div class="card rounded bg-primary text-white p-2 shadow" style="opacity: 0.8" >
+        <i class="m-4 p-2 fa fa-users fa-5x d-flex align-self-center "></i>
+        <div class="h3 p-2   d-flex align-self-center">عدد الزبائن : {{$clients->count()}}</div>
+        <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
+         </div>
         </div>
-        <div class="modal-body">
-          ...
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
-      </div>
+      
+
+        <div class="col col-4 p-4">  <div class="card rounded bg-success text-white p-2 shadow" style="opacity: 0.8" >
+            <i class="m-4 p-2 fa fa-calendar fa-5x d-flex align-self-center "></i>
+            <div class="h3 p-2   d-flex align-self-center">عدد المواعيد  : {{$appointments->count()}}</div>
+            <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
+             </div>
+            </div>
+
+
+
+            <div class="col col-4 p-4">  <div class="card rounded bg-info text-white p-2 shadow" style="opacity: 0.8" >
+                <i class="m-4 p-2 fa fa-list fa-5x d-flex align-self-center "></i>
+                <div class="h3 p-2   d-flex align-self-center">عدد الأنواع : {{$types->count()}}</div>
+                <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
+                 </div>
+                </div>
+
     </div>
-  </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
-</html>
+</div>
+
+
+
+
+
+
+@stop
