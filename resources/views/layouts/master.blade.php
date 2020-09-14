e08ed001518c6e0f192902c691c1aad40616e73b
1 <abdounovich@gmail.com>
2020-09-14 23:13:52 +0200

    'comment'

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,9 +7,9 @@
        
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>      
-        <script src="{{ asset('js/app.js') }}" type="text/js"></script>
+        <script src="{{ asset('js/app.js',true )}}" type="text/js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/app.css',true) }}" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -52,7 +52,7 @@
 
 
      
-<div class="p-2 m-5  align-content-center align-items-center align-self-center"> <a class="btn  btn-dark rounded-circle text-success " ><i class=" p-1 fa fa-bars fa-4x " data-toggle="collapse" data-target="#demo"></i>
+<div class="p-2 m-5  align-content-center align-items-center align-self-center"> <a class="btn btn-dark rounded-circle text-success " ><i class=" p-1 fa fa-bars fa-4x " data-toggle="collapse" data-target="#demo"></i>
 </a></div>
        
           <div id="demo" class="collapse m-5 p-4">
@@ -69,11 +69,6 @@
             
 
 
-            <script>
-             $('#button').click(function() {
-    location.reload();
-});
-            </script>
 
           </div>
             </div>
