30c332800fe128c5bd6a2708b2ddb029882e3df2
1 <abdounovich@gmail.com>
2020-09-15 01:16:12 +0200

    'comment'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,7 +17,7 @@
 <div class="container">
     <div class="row">
 
-        <div class="col col-4 p-4">  <div class="card rounded bg-primary text-white p-2 shadow" style="opacity: 0.8" >
+        <div class="col col-4 ">  <div class="card rounded bg-primary text-white p-2 shadow" style="opacity: 0.8" >
         <i class="m-4 p-2 fa fa-users fa-5x d-flex align-self-center "></i>
         <div class="h3 p-2   d-flex align-self-center">عدد الزبائن : {{$clients->count()}}</div>
         <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
@@ -25,7 +25,7 @@
         </div>
       
 
-        <div class="col col-4 p-4">  <div class="card rounded bg-success text-white p-2 shadow" style="opacity: 0.8" >
+        <div class="col col-4 ">  <div class="card rounded bg-success text-white p-2 shadow" style="opacity: 0.8" >
             <i class="m-4 p-2 fa fa-calendar fa-5x d-flex align-self-center "></i>
             <div class="h3 p-2   d-flex align-self-center">عدد المواعيد  : {{$appointments->count()}}</div>
             <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
@@ -34,7 +34,7 @@
 
 
 
-            <div class="col col-4 p-4">  <div class="card rounded bg-info text-white p-2 shadow" style="opacity: 0.8" >
+            <div class="col col-4 ">  <div class="card rounded bg-info text-white p-2 shadow" style="opacity: 0.8" >
                 <i class="m-4 p-2 fa fa-list fa-5x d-flex align-self-center "></i>
                 <div class="h3 p-2   d-flex align-self-center">عدد الأنواع : {{$types->count()}}</div>
                 <button class=" m-4 p-2 btn btn-warning d-flex align-self-center">مشاهدة الجميع</button>
