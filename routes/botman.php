a9d50de4a409804db753a71d466deba6222cf3ca
1 <abdounovich@gmail.com>
2020-09-15 21:49:49 +0200

    'comment'

--- routes/botman.php
+++ routes/botman.php
@@ -178,7 +178,7 @@ $botman->hears('menu', function ($bot) {
     $bot->reply(ButtonTemplate::create('  الرجاء إختيار زر من القائمة 👇👇 ')
 	->addButton(ElementButton::create(' 📅 مواعيدي')
     ->url($this->config.'/client/'.$DbUsername->slug)
-    ->heightRatio('compact')
+    ->heightRatio('tall')
 
 	)
 	->addButton(ElementButton::create(' 🎁 نقاطي')
