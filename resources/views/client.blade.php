4f7c7fbaca897f6166d99c012832dce1105f72f5
1 <abdounovich@gmail.com>
2020-09-21 22:50:03 +0200

    'comment'

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -163,78 +163,8 @@ switch ($yawm) {
 
     
     $('#countdown-container').ClassyCountdown({
-
-// flat-colors, flat-colors-wide, flat-colors-very-wide, 
-// flat-colors-black, black, black-wide, black-very-wide, 
-// black-black, white, white-wide, 
-// white-very-wide or white-black
-    theme: "white",
-
-// end time
-    end: $.now() + str+360000,
-    now: $.now(),
-
-// whether to display the days/hours/minutes/seconds labels.
-    labels: true,
-
-// object that specifies different language phrases for says/hours/minutes/seconds as well as specific CSS styles.
-    labelsOptions: {
-        lang: {
-            days: 'Days',
-            hours: 'Hours',
-            minutes: 'Minutes',
-            seconds: 'Seconds'
-        },
-        style: 'font-size: 0.5em;'
-    },
-
-// custom style for the countdown
-    style: {
-        element: '',
-        labels: false,
-        textCSS: '',
-        days: {
-            gauge: {
-                thickness: 0.02,
-                bgColor: 'rgba(0, 0, 0, 0)',
-                fgColor: 'rgba(0, 0, 0, 1)',
-                lineCap: 'butt'
-            },
-            textCSS: ''
-        },
-        hours: {
-            gauge: {
-                thickness: 0.02,
-                bgColor: 'rgba(0, 0, 0, 0)',
-                fgColor: 'rgba(0, 0, 0, 1)',
-                lineCap: 'butt'
-            },
-            textCSS: ''
-        },
-        minutes: {
-            gauge: {
-                thickness: 0.02,
-                bgColor: 'rgba(0, 0, 0, 0)',
-                fgColor: 'rgba(0, 0, 0, 1)',
-                lineCap: 'butt'
-            },
-            textCSS: ''
-        },
-        seconds: {
-            gauge: {
-                thickness: 0.02,
-                bgColor: 'rgba(0, 0, 0, 0)',
-                fgColor: 'rgba(0, 0, 0, 1)',
-                lineCap: 'butt'
-            },
-            textCSS: ''
-        }
-    },
-
-// callback that is fired when the countdown reaches 0.
-    onEndCallback: function () {
-    }
-
+    theme: "white", // theme
+    end: $.now() + 645600 // end time
 });</script>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
