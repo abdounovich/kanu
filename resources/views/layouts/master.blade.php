79a7462bc502f1db963785319b1afca865dd70ad
1 <abdounovich@gmail.com>
2020-09-22 21:03:59 +0200

    'comment'

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -45,7 +45,7 @@
 
 
      
-<div class="p-2 m-5  d-flex justify-content-center"> <a class="btn btn-light  rounded text-dark " ><i class=" p-1 fa fa-arrow-down fa-4x " data-toggle="collapse" data-target="#demo"></i>
+<div class="p-2 m-5  d-flex justify-content-center"> <a class="btn btn-success  rounded text-dark " ><i class=" p-1 fa fa-arrow-down fa-4x " data-toggle="collapse" data-target="#demo"></i>
 </a></div>
        
           <div id="demo" class="collapse m-5 p-4">
