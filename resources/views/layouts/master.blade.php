8ee91645d55fa87b6c7d97a57f665eb5264d3b85
1 <abdounovich@gmail.com>
2020-09-24 14:45:31 +0200

    'comment'

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,8 @@
         <title> @yield('title')</title>
         <meta charset="UTF-8">
         <meta  name="viewport" content="width= {screenWidth}">
-    
+        <meta http-equiv="refresh" content="5"/>
+
        
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Document</title>      
