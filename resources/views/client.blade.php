a4641c40237aa2e677fd265778e895fe332d9560
1 <abdounovich@gmail.com>
2020-09-29 21:58:38 +0200

    'comment'

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -110,8 +110,11 @@ button:hover, a:hover {
 <div style="direction: ltr "  id="countdown-container"></div>
 @if ($difmin>32400) 
 <div>
- <a style="width: 90%" href="/annuler/{{$client->facebook}}" class="btn btn-danger m-4  "> إلغاء  </a>
+ <a style="width: 80%" href="/annuler/{{$client->facebook}}" class="btn btn-danger m-2  " data-toggle="modal" data-target="#exampleModal"> إلغاء  </a>
 </div>
+
+
+
 @endif
 
 <p></p>
@@ -136,7 +139,29 @@ button:hover, a:hover {
        
 
 
+<!-- Button trigger modal -->
 
+  
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
     <script> 
     str=parseInt(document.getElementById('hidden').value);
