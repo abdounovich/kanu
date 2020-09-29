9723b57a83e819b21421819ce9a9acfff9c38379
1 <abdounovich@gmail.com>
2020-09-29 17:36:54 +0200

    'comment'

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -116,7 +116,7 @@ button:hover, a:hover {
 إلغاء الموعد    </button>
   
   <!-- Modal -->
-  <div class="modal fade m-2 bg-dark " style="opacity:1" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal fade m-2 bg-dark " style="opacity:1" id="exampleModal" tabindex="1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
