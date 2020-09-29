c51b94a5e19162d6f118063682f9072d1c91c945
1 <abdounovich@gmail.com>
2020-09-29 15:49:13 +0200

    'comment'

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -107,7 +107,7 @@ button:hover, a:hover {
 
                 <div class="m-3">الوقت المتبقي لموعدك : </div>
                 
-                <div   dir="rtl">يوم  {{$yawm}}  على <span style="font-size: 20px;" class="badge badge-warning font-weight-bold p-2">{{$appointment->debut}}  </span>    </div>
+                <div   dir="rtl">يوم  {{$yawm}}  على <span style="font-size: 18px;" class="badge badge-warning  text-white font-weight-bold p-2">{{$appointment->debut}}  </span>    </div>
 
 <div style="direction: ltr "  id="countdown-container"></div>
 
