afe69ecd603a9c26114dd644a9f86ea8d3cb4693
1 <abdounovich@gmail.com>
2020-09-29 15:57:11 +0200

    'comment'

--- resources/views/client.blade.php
+++ resources/views/client.blade.php
@@ -103,7 +103,7 @@ button:hover, a:hover {
            
 
         
-        <div   dir="rtl" class="p-2 m-2"> موعدك</div>
+        <div   dir="rtl" class="p-2 mt-2"> موعدك</div>
 
                 
                 <div   dir="rtl" class="p-2 m-2">  <span style="font-size: 18px;" class="badge badge-warning  text-white font-weight-bold p-2">{{$yawm}}  على {{$appointment->debut}}  </span>    </div>
