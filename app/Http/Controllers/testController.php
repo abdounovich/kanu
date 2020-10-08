6f7aa88421bc708de4aa4946d833e55b54a4b872
1 <abdounovich@gmail.com>
2020-10-08 22:01:02 +0200

    'comment'

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -346,15 +346,19 @@ $date=date("l");
             
             foreach ($Tomorrow_appointments as $appointment ) {    
 
-    $d=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->debut.":00"));
-    $f=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->fin.":00"));
+    echo $d=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->debut.":00"));
+    echo"<br>";
+    echo $f=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->fin.":00"));
+    echo"<br>";
 
               for ($i=0; $i <count($arr) ; $i++) { 
               
               $ai=Carbon::createFromFormat('Y-m-d H:i:s', $arr[$i]); 
               $av=$ai->toDateTimeString();
                $ai->addMinutes(90); 
-              echo $av;  
+              echo $av; 
+              echo"<br>";
+ 
 
 
 
