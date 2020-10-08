6a0f607bbe34aafa67bfee4784743434af8c4682
1 <abdounovich@gmail.com>
2020-10-08 21:59:12 +0200

    'comment'

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -345,14 +345,17 @@ $date=date("l");
           if (count($Tomorrow_appointments)>0) {
             
             foreach ($Tomorrow_appointments as $appointment ) {    
+
+    $d=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->debut.":00"));
+    $f=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->fin.":00"));
+
               for ($i=0; $i <count($arr) ; $i++) { 
               
               $ai=Carbon::createFromFormat('Y-m-d H:i:s', $arr[$i]); 
               $av=$ai->toDateTimeString();
                $ai->addMinutes(90); 
               echo $av;  
-    $d=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->debut.":00"));
-    $f=date("Y-m-d H:i:s", strtotime($appointment->jour." ".$appointment->fin.":00"));
+
 
 
     
