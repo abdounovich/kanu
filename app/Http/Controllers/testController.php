9f9409f1a1db980c690544f4fe48bac92861cc29
1 <abdounovich@gmail.com>
2020-10-08 22:21:56 +0200

    'comment'

--- app/Http/Controllers/testController.php
+++ app/Http/Controllers/testController.php
@@ -334,7 +334,7 @@ $date=date("l");
     $f_pause=$jour." ".$f_pause.":00";
     $f_pause=date("Y-m-d H:i:s", strtotime(date($f_pause))); 
 
-    $Tomorrow_appointments=Appointment::whereJour("2020-10-09")->where('ActiveType',"1")->Orwhere('ActiveType',"2")->get();
+    $Tomorrow_appointments=Appointment::whereJour($jour)->where('ActiveType',"1")->Orwhere('ActiveType',"2")->get();
     while ($debut < $fin )
     {
       $arr[]=$debut;  
@@ -348,7 +348,7 @@ $date=date("l");
               $ai=Carbon::createFromFormat('Y-m-d H:i:s',$key); 
               $ai->toDateTimeString();
               $av=$ai->addMinutes(90); 
-             
+           
             
             foreach ($Tomorrow_appointments as $appointment ) {    
 
