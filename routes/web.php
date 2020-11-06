f2275898787dc5405b35bb008eaafb5b6d646aac
1 <abdounovich@gmail.com>
2020-11-06 17:26:56 +0100

    'comment'

--- routes/web.php
+++ routes/web.php
@@ -16,42 +16,58 @@ Route::get('/add','AppointmentController@AddPFunction');
 Route::get('/tester','HomeController@func');
 
 Auth::routes();
-Route::match(['get', 'post'], '/botman', 'BotManController@handle')->middleware('auth');;
+Route::match(['get', 'post'], '/botman', 'BotManController@handle')->middleware('auth');
 
-Route::get('/botman/tinker', 'BotManController@tinker')->middleware('auth');;
-Route::get('/rdv','AppointmentController@index')->middleware('auth');;
-Route::get('/client/{slug}','ClientController@index')->middleware('auth');;
-Route::get('/clients','ClientController@show')->middleware('auth');;
+Route::get('/botman/tinker', 'BotManController@tinker')->middleware('auth');
+Route::get('/rdv','AppointmentController@index')->middleware('auth');
+Route::get('/client/{slug}','ClientController@index')->middleware('auth');
+Route::get('/clients','ClientController@show')->middleware('auth');
 
-Route::get('/delete','AppointmentController@deleteFunction');
-Route::post('/addAppoint','AppointmentController@store');
+Route::get('/delete','AppointmentController@deleteFunction')->middleware('auth');
+Route::post('/addAppoint','AppointmentController@store')->middleware('auth');
 
-
-Route::post('/types','TypeController@store');
-Route::get('/types','TypeController@index');
-Route::get('/test/{type}/D1/{username}/{Cid}','testController@today');
-Route::get('/test/{type}/D2/{username}/{Cid}','testController@tomorrow');
-Route::get('/test/{type}/D3/{username}/{Cid}','testController@afterTomorrow');
-Route::get('/settings','SettingController@index');
-Route::post('/settings','SettingController@store');
-Route::post('/settings/{id}','SettingController@update');
-
-
-
-
-Route::post('/test2','testController@sendTextMessage');
-
-Route::post('/test','testController@commande');
-
-Route::get('/delete/{id}','TypeController@supprimer');
-Route::get('/edit/{id}','TypeController@edit');
-Route::post('/types_edit/{id}','TypeController@update');
-Route::get('/actif/{id}/{num}','AppointmentController@actif');
-Route::post('/annuler','AppointmentController@Annuler');
-Route::get('/annulerByAdmin/{id}','AppointmentController@AnnulerByAdmin');
+Route::post('/types','TypeController@store')
+->middleware('auth');
+Route::get('/types','TypeController@index')
+->middleware('auth');
+Route::get('/test/{type}/D1/{username}/{Cid}','testController@today')
+->middleware('auth');
+Route::get('/test/{type}/D2/{username}/{Cid}','testController@tomorrow')
+->middleware('auth');
+Route::get('/test/{type}/D3/{username}/{Cid}','testController@afterTomorrow')
+->middleware('auth');
+Route::get('/settings','SettingController@index')
+->middleware('auth');
+Route::post('/settings','SettingController@store')
+->middleware('auth');
+Route::post('/settings/{id}','SettingController@update')
+->middleware('auth');
 
 
-Route::get('/','HomeController@index');
+
+
+Route::post('/test2','testController@sendTextMessage')
+->middleware('auth');
+
+Route::post('/test','testController@commande')
+->middleware('auth');
+
+Route::get('/delete/{id}','TypeController@supprimer')
+->middleware('auth');
+Route::get('/edit/{id}','TypeController@edit')
+->middleware('auth');
+Route::post('/types_edit/{id}','TypeController@update')
+->middleware('auth');
+Route::get('/actif/{id}/{num}','AppointmentController@actif')
+->middleware('auth');
+Route::post('/annuler','AppointmentController@Annuler')
+->middleware('auth');
+Route::get('/annulerByAdmin/{id}','AppointmentController@AnnulerByAdmin')
+->middleware('auth');
+
+
+Route::get('/','HomeController@index')
+->middleware('auth');
 
 Route::get('/commande', function () {
     return view('commande') ;
@@ -62,8 +78,10 @@ Route::get('/t', function () {
     return view('tester') ;
 });
 Route::get('/abcd','TestController@try');
-Route::post('/sendMsg/{id}','ClientController@sendMessageToClient');
-Route::get('/sendMsg/{id}','ClientController@sendMessageToClientView');
+Route::post('/sendMsg/{id}','ClientController@sendMessageToClient')
+->middleware('auth');
+Route::get('/sendMsg/{id}','ClientController@sendMessageToClientView')
+->middleware('auth');
 
 
 
