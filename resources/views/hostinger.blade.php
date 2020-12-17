3c9b11df8dbc6971525f45baa10babc2d346a658
1 <abdounovich@gmail.com>
2020-12-17 20:56:18 +0100

    'comment'

--- resources/views/hostinger.blade.php
+++ resources/views/hostinger.blade.php
@@ -9,11 +9,233 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <title>Hello, world!</title>
+    <style>body {
+        padding-top: 20px;
+    }
+    .delete-item {
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+    .accordion-item {
+        border-bottom: 2px dashed #ccc;
+        padding-bottom: 10px;
+        margin-bottom: 20px;
+        position: relative;
+    }
+    .add-new-wrapper {
+        border-bottom: 2px dashed #ccc;
+        padding-bottom: 20px;
+    }
+    .clear-both {
+        clear: both;
+    }
+    .accordion-preview-wrapper {
+        display: none;
+    }
+    .panel-heading {
+        padding: 0;
+    }
+    .panel-heading a {
+        padding: 10px 15px;
+        display: block;
+    }
+    .panel-title i {
+        float: right;
+    }</style>
   </head>
   <body>
 
-    <a href="/add" class="btn btn-primary"  type="button">Text</a>
-
+  
+    <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading1">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse1" aria-expanded="true" aria-controls="collapse1" class="collapsed">
+                        <span>INTRODUCE YOURSELF</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading1">
+                <div class="panel-body">
+                    First of all, thank you very much for giving me this opportunity to introduce myself.
+    My name is ABDELDJALIL MERAHI and I am from Algeria. I’m 25 years old I graduated last year from the Hight national school of biotechnology, with engineer and master degree in Medical biotechnology.
+    In the meantime, I am a qualification & validation engineer at HUPPharma company and I’m responsible for the HVAC systems, Utility and cleaning validation management.
+    I have been working with web development since 2015 as freelancer and in different environments, from big consultancy companies to startups.
+    I am also a software and android app developer I’ve built a lot of personnel projects.
+    currently, I’m working on a barbershop Appointment Scheduler application associated with messenger chatbot using Botman studio and facebookapi graph.
+    I am creative and self-motivated person who likes to solve problems and communicate with other people from all around the world.
+    My repertoire includes programming languages and tools such as php javascript , java, C ,Python, VueJS, Laravel, Wordpress, MySQL, PostgreSQL, github, Heroku app deployment, Apache server configuration, Bootstrap, jQuery and much more.
+    
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading2">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse2" aria-expanded="true" aria-controls="collapse2" class="collapsed">
+                        <span>How did hear about us?</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2">
+                <div class="panel-body">
+                    In fact, a friend of mine who suggested me to apply for this position, currently she is a customer success manager at hostinger and she is always talking about the great team and the wonderful atmosphere they are.
+     I like it so much and I am really so excited to join you.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading3">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse3" aria-expanded="true" aria-controls="collapse3" class="collapsed">
+                        <span>what do you know about hostinger?</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3">
+                <div class="panel-body">
+                     I’ve had a look at your website so my understanding that hostinger is a web hosting provider and internet domain registrar. The company uses cloud web hosting technology and provides hosting with MySQL, FTP and PHP. Founded in Kaunas, Lithuania in 2004 and on 2020 Hostinger was listed as one of the fastest growing companies in Europe.
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading4">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse4" aria-expanded="true" aria-controls="collapse4" class="collapsed">
+                        <span>WHY ME</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading4">
+                <div class="panel-body">
+                    Honestly, I possess all the skills and experience that you’re looking for. I’m pretty confident that I am the best candidate for this position.
+    Over the years, I have acquired relevant skills and experience, which I shall bring to hostinger. I have also worked tirelessly on my communication abilities and teamwork skills. I will put to use in my future career, which would be in your company if I am get selected for the position.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading5">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse5" aria-expanded="true" aria-controls="collapse5" class="collapsed">
+                        <span>Solving customers issues:</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse5" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading5">
+                <div class="panel-body">
+                    The first thing to do is to listen carefully to what the customers have to say, I must put myself in their places because they need to feel like a I am on their side and that I empathize with the situation. 
+    I let them finish then I start asking the right questions to Identify the problem and analyze it. 
+    After that I develop multiple solutions and I don't stop at the first one. It may be good, but much better ones may exist.
+    The final step consists on choosing the optimal solution and start solving the problem or find someone who can solve it and quickly.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading6">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse6" aria-expanded="true" aria-controls="collapse6" class="collapsed">
+                        <span>MY WEAKNESS and Strenths</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading6">
+                <div class="panel-body">
+                    
+    The one weakness that I have is I don't have much experience in this type of work directly. but I have many experiences with costumers as a freelancer because achieving their success is a common goal.
+    ....
+    I consider my Patience .and problem-solving skills to be one of my greatest strengths. During my time as a freelancer, I successfully worked on more than one different project at a time. As a result, I have merged them all in a single template for my next project.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading7">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse7" aria-expanded="true" aria-controls="collapse7" class="collapsed">
+                        <span>HUPP >>></span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading7">
+                <div class="panel-body">
+                    
+    In fact, one of the reasons I’m looking for a new opportunity is to expand my knowledge and I’d like to have a platform where I could share my ideas and offer up ways to improve my technical skills and communicate with other People, things that I didn't find in my current job.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading8">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse8" aria-expanded="true" aria-controls="collapse8" class="collapsed">
+                        <span>TEAM WORK:</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse8" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading8">
+                <div class="panel-body">
+                    
+    I’m a “people person” – I enjoy working with others, and I know how to communicate well, I’m really energized by team dynamics and excited as I witness the progress we make towards our goals.
+    I prefer to work as a team member, because I believe that the best ideas are developed in partnership with others. I’m equally comfortable being a team member and why not a team leader.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading9">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse9" aria-expanded="true" aria-controls="collapse9" class="collapsed">
+                        <span>AFTER X YEAR WHERE DO YOU SEE YOURSELF</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse9" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading9">
+                <div class="panel-body">
+                    
+    From the moment I read the job description for this position I was really excited, and for this reason I’m really driven to achieve both mine and the company’s goals, and it is my belief that this stability will allow me to do so as I grow with the team.  X years down the road I see myself growing into a supervisor or manager where I’ll be able to use my skills to support and influence others.
+    
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading10">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse10" aria-expanded="true" aria-controls="collapse10" class="collapsed">
+                        <span>What Does Customer success means to You?</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse10" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading10">
+                <div class="panel-body">
+                    for  me , customer success is  ensuring that every customer receives the  maximum value from the product or service. as a result of that , we maximize retention, preventing churn, and ensuring customer happiness.
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading" role="tab" id="heading11">
+                <h3 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion1" href="#collapse11" aria-expanded="true" aria-controls="collapse11" class="collapsed">
+                        <span>QUESTIONS:</span> <i class="glyphicon glyphicon-plus"></i>
+                    </a>
+                </h3>
+            </div>
+            <div id="collapse11" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading11">
+                <div class="panel-body">
+                    
+    1- can you tell me a little more  about each member of the team?
+    2- What are the next steps in the interview process?
+    3- Is there anything else I can provide you with that would be helpful or questions I can answer?
+    4- How will I be trained?
+    
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
