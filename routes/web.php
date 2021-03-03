e012a9db38b6235449ab64fb387ffa17e1306315
1 <abdounovich@gmail.com>
2021-03-03 20:22:28 +0100

    comment

--- routes/web.php
+++ routes/web.php
@@ -81,21 +81,17 @@ Route::get('/22', function () {
     $config=Config::get('botman.facebook.token');
 
 
-    try {
-        ini_set("allow_url_fopen", 1);
+    if ($userInfoData===FALSE) {
+      
+    }else{  ini_set("allow_url_fopen", 1);
         $text='https://graph.facebook.com/v2.6/3582653051862423?fields=profile_pic&access_token='.$config;
                       $userInfoData=file_get_contents($text);
                       $userInfo = json_decode($userInfoData, true);
                  echo  $picture = $userInfo['profile_pic'] ;
-      } catch(FacebookExceptionsFacebookResponseException $e) {
-        echo 'Graph returned an error: ' . $e->getMessage();
-        exit;
-      } catch(FacebookExceptionsFacebookSDKException $e) {
-        echo 'Facebook SDK returned an error: ' . $e->getMessage();
-        exit;
-      }
+    
 
-   
+   }
+      
     
 });
 
