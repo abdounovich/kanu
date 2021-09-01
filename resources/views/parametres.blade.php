716bfc177d756ec5d2ff6b216f66c443e8f3a351
abdounovich <30698178+abdounovich@users.noreply.github.com>
2021-09-01 12:11:11 +0200

    Update parametres.blade.php

--- resources/views/parametres.blade.php
+++ resources/views/parametres.blade.php
@@ -113,5 +113,61 @@
 
 </div>
 
- 
+ <form action="/parametres/update/" method="POST">
+    <table class="table  table-bordred">
+    <tr class="bg-dark text-white text-center">
+        <td>اليوم </td>
+        <td>البداية </td>
+        <td>النهاية </td>
+        <td>بداية الراحة </td>
+        <td>نهاية الراحة </td>
+        <td>الحالة </td>
+    </tr>
+    @php
+    $arabic = ['السبت' ,'الأحد','الإثنين','الثلاثاء','الأربعاء','الخميس','الجمعة' ]; 
+    $anglais = ['Saturday' ,'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday' ];  
+   
+    @endphp
+  
+@csrf @for ($i = 0; $i < 7; $i++)
+<tr class="bg-secondary text-white text-center">
+        <td>{{$arabic[$i]}}</td>
+        <td><input class="form-control {{$anglais[$i]}} " @if (Setting::get($anglais[$i].'.active')=="0" ) readonly @endif type="time" name="{{$anglais[$i].'-debut'}}" value="{{Setting::get($anglais[$i].'.debut')}}"></td>
+        <td><input class="form-control {{$anglais[$i]}} "@if (Setting::get($anglais[$i].'.active')=="0" ) readonly @endif type="time" name="{{$anglais[$i].'-fin'}}" value="{{Setting::get($anglais[$i].'.fin')}}"></td>
+        <td><input class="form-control {{$anglais[$i]}}"@if (Setting::get($anglais[$i].'.active')=="0" ) readonly @endif type="time" name="{{$anglais[$i].'-debut-repos'}}" value="{{Setting::get($anglais[$i].'.debut-repos')}}"></td>
+        <td><input class="form-control {{$anglais[$i]}} " @if (Setting::get($anglais[$i].'.active')=="0" ) readonly @endif type="time" name="{{$anglais[$i].'-fin-repos'}}" value="{{Setting::get($anglais[$i].'.fin-repos')}}"></td>
+        <td><input  class="" type="checkbox"    
+            @if (Setting::get($anglais[$i].'.active')=="1" ) checked  
+            @endif 
+            onchange="myFunction('{{$i}}')" id="cb{{$i}}" data-on="مفعل" 
+            data-off="موقف" data-onstyle="outline-success" data-offstyle="outline-danger"  data-toggle="toggle"></td>
+  <input type="hidden" id="{{$i}}" name="{{$anglais[$i].'-active'}}" value="{{Setting::get($anglais[$i].'.active')}}">
+      </tr>
+@endfor
+   
+</table>
+<div class="col col-12 text-center ">
+    <button style="width: 100%" type="submit" class="btn btn-success">  حفظ التغييرات </button>
+ </div>
+ </form>
+</div>
+<script>
+    function myFunction($id) {
+    var checkbox=document.getElementById("cb"+$id);
+    var hidden_input=document.getElementById($id);
+  
+     
+  /*     window.location = "/actif/"+$fid;
+   */  
+      
+  
+   if (checkbox.checked == true){
+    hidden_input.value="1";
+    } else {
+        hidden_input.value="0";
+   }
+  
+  }
+  
+  ;</script> 
 @stop
